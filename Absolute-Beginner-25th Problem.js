659ea7e2c6993aa63e7b432846394ee81f25c04e
Voleti Sailaja <81481090+voletisailaja@users.noreply.github.com>
2023-03-22 00:00:50 +0530

    Create Absolute-Beginner-25th Problem.js

---
+++ Absolute-Beginner-25th Problem.js
@@ -0,0 +1,25 @@
+ var n=userInput[0].split(" ");
+    var n1=parseInt(n[0]);
+    var n2=parseInt(n[1]);
+    var arr1=[]; 
+    var arr2=[];
+    
+    for(i=1;i<=n1;i++){
+        if(n1%i==0)
+            arr1.push(i)
+    }
+    
+    for(i=1;i<=n2;i++){
+        if(n2%i==0)
+            arr2.push(i)
+    }
+   
+    var factor=0
+    for(i=0;i<arr1.length&&i<arr2.length;i++){
+        if(arr2.includes(arr1[i]))
+            factor=arr1[i];
+    }
+    
+    console.log(factor)
+
+  //end-here
